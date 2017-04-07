4e4c792de834d14c605d3b2ba78165c17a75adb2
Andrew Byrd <Byrd33@live.missouristate.edu>
2017-04-07 13:50:05 -0500

    Fix routes

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -8,9 +8,7 @@
 export default {
   name: 'hello',
   data () {
-    return {
-      
-    }
+    return {}
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Upload from '@/components/Upload'
+import Permissions from '@/components/Permissions'
+import Admin from '@/components/Admin'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/upload',
       name: 'Upload',
       component: Upload
+    },
+    {
+      path: '/permissions',
+      name: 'Permissions',
+      component: Permissions
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin
     }
   ]
 })
